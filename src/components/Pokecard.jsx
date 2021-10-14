296fb28942396420d9f47657475c009b178d77de
Vimal M <vimalnavee2110@gmail.com>
2021-10-14 19:22:24 +0530

    Pokegame

--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import "./Pokecard.css"
 
+
 class Pokecard extends Component {
+
     render() {
-        let img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.prop.id}.png`
+        let img = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+        let padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3) : num);
+        let imgsrc = `${img}${padToThree(this.props.prop.id)}.png`;
+
         return (
             <div className="card">
                 <h3>{this.props.prop.name}</h3>
-                <img src={img} alt="" srcset="" />
+                <img src={imgsrc} alt="" srcset="" />
                 <div>
                     <p>type:{this.props.prop.type}</p>
                     <p>EXP:{this.props.prop.base_experience}</p>
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -6,22 +6,13 @@ import "./Pokedex.css";
 class Pokedex extends Component {
 
     render() {
-        let arr = [
-            { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-            { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-            { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-            { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-            { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-            { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-            { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-            { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
-        ]
-        let ele = arr.map(ele => {
+        let ele = this.props.prop.map(ele => {
             return <Pokecard prop={ele} />
         })
         return (
             <div className="pokedex">
-                <h1>Pokedex</h1>
+                <h3 className={this.props.isWinner ? 'win' : 'lose'}>{this.props.isWinner ? 'Winning Hand' : 'Loosing Hand'}</h3>
+                <p>Total Experience : {this.props.exp}</p>
                 <div className="pokecard_container">
                     {ele}
                 </div>
